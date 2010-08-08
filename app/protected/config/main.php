f25252808ac9e6e31ad68033698694be53f2c090
Kenneth Scott <kscott@churchcommunitybuilder.com>
2010-08-08 14:54:27 -0600

    Fix formatting to match standards

--- app/protected/config/main.php
+++ app/protected/config/main.php
@@ -6,43 +6,43 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'CCB Checkin Demo',
+	'basePath' => dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name' => 'CCB Checkin Demo',
 
 	// preloading 'log' component
-	'preload'=>array('log'),
+	'preload' => array('log'),
 
 	// autoloading model and component classes
-	'import'=>array(
+	'import' => array(
 		'application.models.*',
 		'application.components.*',
 	),
 
 	// application components
-	'components'=>array(
-		'user'=>array(
+	'components' => array(
+		'user' => array(
 			// enable cookie-based authentication
-			'allowAutoLogin'=>true,
+			'allowAutoLogin' => true,
 		),
 		// uncomment the following to enable URLs in path-format
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-	            'gii'=>'gii',
-	            'gii/<controller:\w+>'=>'gii/<controller>',
-	            'gii/<controller:\w+>/<action:\w+>'=>'gii/<controller>/<action>',
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+		'urlManager' => array(
+			'urlFormat' => 'path',
+			'rules' => array(
+	            'gii' => 'gii',
+	            'gii/<controller:\w+>' => 'gii/<controller>',
+	            'gii/<controller:\w+>/<action:\w+>' => 'gii/<controller>/<action>',
+				'<controller:\w+>/<id:\d+>' => '<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+				'<controller:\w+>/<action:\w+>' => '<controller>/<action>',
 			),
 		),
 		/*
-		'db'=>array(
+		'db' => array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		*/
 		// uncomment the following to use a MySQL database
-		'db'=>array(
+		'db' => array(
 			'connectionString' => 'mysql:host=localhost;dbname=ccb_dev',
 			'emulatePrepare' => true,
 			'username' => 'root',
@@ -51,7 +51,7 @@ return array(
 			'enableProfiling' => true,
 			'enableParamLogging' => true,
 		),
-		'ccb'=>array(
+		'ccb' => array(
 			'class' => 'CDbConnection',
 			'connectionString' => 'mysql:host=localhost;dbname=ccbcompany',
 			'emulatePrepare' => true,
@@ -61,46 +61,46 @@ return array(
 			'enableProfiling' => true,
 			'enableParamLogging' => true,
 		),
-		'errorHandler'=>array(
+		'errorHandler' => array(
 			// use 'site/error' action to display errors
-            'errorAction'=>'site/error',
+            'errorAction' => 'site/error',
         ),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
+		'log' => array(
+			'class' => 'CLogRouter',
+			'routes' => array(
 				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
+					'class' => 'CFileLogRoute',
+					'levels' => 'error, warning',
 				),
 				// uncomment the following to show log messages on web pages
 				array(
-					'class'=>'CWebLogRoute',
-					'showInFireBug'=>true,
-					'levels'=>'trace,info, error, warning',
+					'class' => 'CWebLogRoute',
+					'showInFireBug' => true,
+					'levels' => 'trace,info, error, warning',
 				),
 				// array(
-				// 	'class'=> 'CProfileLogRoute',
+				// 	'class' =>  'CProfileLogRoute',
 				// 	'showInFireBug' => true,
-				// 	'levels'=>'trace,info, error, warning',
+				// 	'levels' => 'trace,info, error, warning',
 				// ),
 			),
 		),
 	),
 
-	'modules'=>array(
-	    'gii'=>array(
-	        'class'=>'system.gii.GiiModule',
-	        'password'=>'checkin',
-	        // 'ipFilters'=>array(...a list of IPs...),
-	        // 'newFileMode'=>0666,
-	        // 'newDirMode'=>0777,
+	'modules' => array(
+	    'gii' => array(
+	        'class' => 'system.gii.GiiModule',
+	        'password' => 'checkin',
+	        // 'ipFilters' => array(...a list of IPs...),
+	        // 'newFileMode' => 0666,
+	        // 'newDirMode' => 0777,
 	    ),
 	),
 
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
-	'params'=>array(
+	'params' => array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail' => 'webmaster@example.com',
 	),
 );
