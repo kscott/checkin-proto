2f792be32eb6544d6ae2585da84951bce60b55ab
Corey Winkelmann <cwinkelmann@churchcommunitybuilder.com>
2010-08-13 22:14:11 -0600

    [issue-4] Added the main checkin controller and view layout.
    Added the checkinMain.css to handle the other layout with the checkin
    wrapper and controller. This will handle the initial search and the page
    after the search when selecting who to checkin where.

---
+++ app/protected/views/checkin/index.php
@@ -0,0 +1,48 @@
+<div id="inner-title">Search</a></div>
+
+<form autocomplete="off" action="checkin_family_list.php" autocomplete="off" name="main_form" method="post" class="display:inline;">
+	<input type="hidden" name="ax" value="search">
+	<input type="hidden" name="where_to_go" value="family" />
+	
+	<div id="inner-title-sub">
+	<em>Enter One of the following:</em><br/>
+	Phone number (last 4 digits), Last name, Barcode
+	</div>
+	
+	<div id="main-search-container">
+		<div id="main-search">
+			<input type="text" name="search_text" id="search-text" value="" size="35" maxlength="30" /> 
+		</div>
+	</div>
+
+</form>
+
+<table cellspacing="4" cellpadding="2" border="0" align="center" class="f1">
+
+		<form class="display:inline;" method="post" name="report_form" autocomplete="off" action="checkin_checkedin_list.php"></form>
+			<input type="hidden" value="search" name="ax">
+
+			<tbody><tr>
+				<td nowrap="" class="td-label-textarea">
+					&nbsp;
+				</td>
+				<td class="td-input">
+					<br>
+					<select name="event_id">
+						<option selected="" value="ALL">All Groups-Events</option>
+											<option value="776">Ellerbe Road Church - Sunday School</option>
+												<option value="723">PL 2nd-3rd - Free's Attendance</option>
+												<option value="736">PL 3 - 5's - Sunday School</option>
+												<option value="737">PL 4th-5th - Sunday School</option>
+												<option value="778">PL Nursery - Sunday School</option>
+												<option value="777">PromiseLand Weam WWW Breakdown - Sunday School - Western Campus</option>
+						</select>&nbsp;&nbsp;<input type="submit" value="Lookup"><br>
+					<br>
+				</td>
+			</tr>
+
+		
+
+	</tbody>
+</table>
+
---
+++ app/protected/views/layouts/checkin.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
+
+<head>
+	
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+	<META HTTP-EQUIV="Expires" CONTENT="Fri, 26 Mar 1999 23:59:59 GMT">
+		
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/checkinMain.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+	
+	<style type="text/css" media="print">
+		
+		#header {display: none;}
+		#top-nav {display: none;}
+		#page-nav {display: none;}
+		#screen-content-only {display: none;}
+		.print-no {display: none;page-break-after:always;}
+
+	</style>
+	
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-4053439-2']);
+	  _gaq.push(['_setDomainName', '.ccbchurch.com']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+	
+</head>
+
+<body>
+
+<div id="container">
+	
+	<div id="header">
+	</div>
+		
+	<div id="top-nav">
+		<ul class="">
+			<li><a href="checkin_start.php" onclick="return confirm_this('You are about to exit this check-in setup. To re-enter, you will have to reset certain information for this check-in station.');">Redo Setup</a></li>
+			<li><a href="logout.php?checkin=true" onclick="return confirm_this('You are about to logout. This will require an administrator to re-input the login and password for this check-in station.');">Logout</a></li>
+		</ul>
+	</div>
+	
+	<div id="page-nav">
+	</div>
+	
+	<div id="content">
+		<?php echo $content; ?>
+	</div>
+	
+</div>
+
+</body>
+</html>
+
