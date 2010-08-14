8fa29d4972973bb158343d7008dacab4ec6f78be
Corey Winkelmann <cwinkelmann@churchcommunitybuilder.com>
2010-08-14 11:59:40 -0600

    [Issue-4] Small reorder of the html.

--- app/protected/views/checkin/index.php
+++ app/protected/views/checkin/index.php
@@ -17,32 +17,27 @@
 
 </form>
 
-<table cellspacing="4" cellpadding="2" border="0" align="center" class="f1">
-
-		<form class="display:inline;" method="post" name="report_form" autocomplete="off" action="checkin_checkedin_list.php"></form>
-			<input type="hidden" value="search" name="ax">
-
-			<tbody><tr>
-				<td nowrap="" class="td-label-textarea">
-					&nbsp;
-				</td>
-				<td class="td-input">
-					<br>
-					<select name="event_id">
-						<option selected="" value="ALL">All Groups-Events</option>
-											<option value="776">Ellerbe Road Church - Sunday School</option>
-												<option value="723">PL 2nd-3rd - Free's Attendance</option>
-												<option value="736">PL 3 - 5's - Sunday School</option>
-												<option value="737">PL 4th-5th - Sunday School</option>
-												<option value="778">PL Nursery - Sunday School</option>
-												<option value="777">PromiseLand Weam WWW Breakdown - Sunday School - Western Campus</option>
-						</select>&nbsp;&nbsp;<input type="submit" value="Lookup"><br>
-					<br>
-				</td>
-			</tr>
-
-		
-
-	</tbody>
-</table>
-
+<form class="display:inline;" method="post" name="report_form" autocomplete="off" action="checkin_checkedin_list.php"></form>
+	<input type="hidden" value="search" name="ax">
+	<table cellspacing="4" cellpadding="2" border="0" align="center" class="f1">
+				<tbody><tr>
+					<td nowrap="" class="td-label-textarea">
+						&nbsp;
+					</td>
+					<td class="td-input">
+						<br>
+						<select name="event_id">
+							<option selected="" value="ALL">All Groups-Events</option>
+												<option value="776">Ellerbe Road Church - Sunday School</option>
+													<option value="723">PL 2nd-3rd - Free's Attendance</option>
+													<option value="736">PL 3 - 5's - Sunday School</option>
+													<option value="737">PL 4th-5th - Sunday School</option>
+													<option value="778">PL Nursery - Sunday School</option>
+													<option value="777">PromiseLand Weam WWW Breakdown - Sunday School - Western Campus</option>
+							</select>&nbsp;&nbsp;<input type="submit" value="Lookup"><br>
+						<br>
+					</td>
+				</tr>
+		</tbody>
+	</table>
+</form>
